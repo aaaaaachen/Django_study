3871f63d91dec07d5df49f4612006ba14912befd
wc <1195741928@qq.com>
2019-04-16 10:35:20 +0800

    创建app项目模块

---
+++ demo01/booktest/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+'''
+通过少量代码拥有一个强大的后台
+'''
---
+++ demo01/booktest/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+'''
+django 中 MTV 中的 M
+ORM中的O
+需要定义实体类
+'''

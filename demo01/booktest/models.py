8315f6bfc91ab3399566e2dd905b63af52e30afb
wc <1195741928@qq.com>
2019-04-16 11:51:02 +0800

    添加数据库 bookinfo 表 heroinfo 表

--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -7,3 +7,13 @@ django 中 MTV 中的 M
 ORM中的O
 需要定义实体类
 '''
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField()
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=100)
+    hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)

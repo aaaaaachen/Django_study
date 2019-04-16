c4bbae4da3b23d35a6ee9b2d7297f3f11ab2b171
wc <1195741928@qq.com>
2019-04-16 15:58:29 +0800

    admin 后台管理

--- demo01/booktest/admin.py
+++ demo01/booktest/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
+from .models import BookInfo,HeroInfo
 # Register your models here.
 '''
 通过少量代码拥有一个强大的后台
 '''
+
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -12,8 +12,14 @@ class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.btitle
+
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=100)
     hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hname
